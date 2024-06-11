8d820daca81a1232e412c9bd31532b7fd3aab18d
Nkemjika <atuorankemjika@gmail.com>
2024-06-10 21:14:07 -1200

    new commit

---
+++ database/migrations/2024_06_11_085716_create_carts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('productName')->nullable();
+            $table->string('productImage')->nullable();
+            $table->string('unitPrice')->nullable();
+            $table->string('totalPrice')->nullable();
+            $table->string('productQuantity')->nullable();
+            $table->string('name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+};
